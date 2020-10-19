fc2c6b30cd352a6d7399bc3988d37b6f1ca71ea8
Henrique Amaral <58604136+havelar@users.noreply.github.com>
2020-10-19 16:28:36 -0300

    Update examples.py

--- examples.py
+++ examples.py
@@ -1,5 +1,6 @@
 ############# Simple printing #############
 from cprint import cPrint
+print(1*'\n'+'############# Testing Simple printing #############\n')
 
 cPrint('Henrique em vermelho', 'red')
 cPrint('Avelar Bold Blue', 'blue', style='underline')
@@ -7,16 +8,19 @@ cPrint('Amaral subline', 'pink', style='bold')
     
 ############# Save format to be used anywhere #############
 from cprint import cClass
+print(3*'\n'+'############# Testing saved format #############\n')
 
-red = cClass('red', 'white', style='bold', end='')
-blue = cClass('blue', 'white', style='bold')
+red = cClass('red', 'white', style='bold', end=' | ')
+blue = cClass('blue', 'white', style='bold', end=' | ')
 green = cClass('green')
 
 red('Henrique ')
-blue('Amaral')
+blue('Avelar')
+green('Amaral')
 
 ############# Easy Success / Warning / Error printing #############
 from cprint import cWarn, cSuccess, cError
+print(3*'\n'+'############# Testing warnings #############\n')
 
 cSuccess('Worked.')
 cWarn('Might not work, idk yet...')
@@ -24,11 +28,13 @@ cError('This is an error !!')
 
 ############# Printing Script #############
 from cprint import cScript
+print(3*'\n'+'############# Testing script #############\n')
 
 script = "<script>\n$(document).ready(function() {\n  let src  = parseInt('{URLPARAMETERS_source}');\n  if (src == '0') {\n    $('input.text:visible').val('Offerwise');\n  } else {\n    $('input.text:visible').val('Unkown source.');\n  }\n  $('#movenextbtn').click();\n});\n</script>"
 cScript(script, 'black', 'gray', style='bold')
 
 ############# Multi Colored #############
 from cprint import cColor
+print(3*'\n'+'############# Testing Multi Colored #############\n')
 
 print(cColor('Henrique','red', style='bold') +'-'+ cColor('Avelar', 'green', style='bold') +'-'+ cColor('Amaral', 'blue', style='bold'))
